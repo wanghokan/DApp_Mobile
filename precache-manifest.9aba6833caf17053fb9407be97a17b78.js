a3b8dda8b8da3817542fc047a41106fa1911b3c4
unknown <w52806991@gmail.com>
2020-04-11 19:09:29 +0800

    Updates

---
+++ precache-manifest.9aba6833caf17053fb9407be97a17b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7025e78f46a05e21ef291f5edc363c75",
+    "url": "/DApp_Mobile/index.html"
+  },
+  {
+    "revision": "c1d32c5363889c1f3292",
+    "url": "/DApp_Mobile/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "33c935d73708d582d9da",
+    "url": "/DApp_Mobile/static/css/main.d067e633.chunk.css"
+  },
+  {
+    "revision": "c1d32c5363889c1f3292",
+    "url": "/DApp_Mobile/static/js/2.98b7e022.chunk.js"
+  },
+  {
+    "revision": "0318414f8a12eec8e52d076df7b6679d",
+    "url": "/DApp_Mobile/static/js/2.98b7e022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c935d73708d582d9da",
+    "url": "/DApp_Mobile/static/js/main.b23c358d.chunk.js"
+  },
+  {
+    "revision": "a89423e5b04c14bbddfb",
+    "url": "/DApp_Mobile/static/js/runtime-main.ad2abd37.js"
+  }
+]);
